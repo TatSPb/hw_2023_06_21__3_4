26e41c66aa85ea5ea8c023682fc0b51ec01a56ea
TatSPb <t-sverchkova@yandex.ru>
2023-07-26 23:53:38 +0300

    Initial commit

---
+++ src/test/java/hw_2023_06_21__3_4/Hw2023062134ApplicationTests.java
@@ -0,0 +1,13 @@
+package hw_2023_06_21__3_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2023062134ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
